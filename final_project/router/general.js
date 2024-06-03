15278cc806aeade5a91141193a3e899d16fb176f
Abdullah Demir <dem1r28605@gmail.com>
2024-06-03 23:28:39 +0200

    .

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,18 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req, res) => {
-    //Write your code here
-    return res.status(404).json({ message: "Yet to be implemented" });
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      if (!isValid(username)) {
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -39,25 +47,38 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
-    const book = Object.values(books).find(book => book.author === author);
+    const booksByAuthor = Object.values(books).filter(book => book.author === author);
+
+    if (booksByAuthor.length === 0) {
+        return res.status(404).json({ message: "Author not found" });
+    }
+
+    return res.status(200).json(booksByAuthor);
+});
+
+// Get all books based on title
+public_users.get('/title/:title', function (req, res) {
+       const title = req.params.title;
+
+    const book = Object.values(books).find(book => book.title === title);
 
     if (!book) {
-        return res.status(404).json({ message: "Author not found" });
+        return res.status(404).json({ message: "Book not found" });
     }
 
     return res.status(200).json(book);
 });
 
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = Object.values(books).find(book => book.isbn === isbn);
+
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).json(book.reviews);
 });
 
-//  Get book review
-public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
-});
 
 module.exports.general = public_users;
